0ee6886888d8f103534bc1e297f4e8da96941c7e
wallace <wallacegibbon@outlook.com>
2022-10-25 16:28:28 +0800

    Trivial changes: better test code for `Promise.any`. (the last error will be throw out when all promises are rejected)

--- tests/all_race_any.js
+++ tests/all_race_any.js
@@ -3,6 +3,7 @@ import { MiniPromise as Promise } from "../mod.js";
 const p1 = new Promise((res, _) => setTimeout(() => res("p1"), 2000));
 const p2 = new Promise((res, _) => setTimeout(() => res("p2"), 3000));
 const p3 = new Promise((_, rej) => setTimeout(() => rej("p3"), 1000));
+const p4 = new Promise((_, rej) => setTimeout(() => rej("p4"), 4000));
 
 Promise.all([ p1, p2, p3 ]).then(
 	r => console.log(`Promise.all result: ${r}`),
@@ -20,6 +21,11 @@ Promise.race([ p1, p2, p3 ]).then(
 );
 
 Promise.any([ p1, p2, p3 ]).then(
+	r => console.log(`Promise.any result: ${r}`),
+	e => console.error(`Promise.any error: ${e}`),
+);
+
+Promise.any([ p3, p4 ]).then(
 	r => console.log(`Promise.any result: ${r}`),
 	e => console.error(`Promise.any error: ${e}`),
 );
